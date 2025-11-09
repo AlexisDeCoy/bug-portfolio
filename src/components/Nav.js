cc7fc6bddf7913e8b6163bff62678d8212789c36
AlexisDeCoy <alexiscdecicco@gmail.com>
2025-11-09 16:44:51 -0700

    Add Teaching page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,6 +13,7 @@ const Nav = () => {
                     <li className={`${location === '/' ? "active" : ""}`} onClick={() => navigate('/')}>HOME</li>
                     <li className={`${location.includes('/prints') ? "active" : ""}`} onClick={() => navigate('/prints/portfolio')}>PRINTMAKING</li>
                     <li className={`${location.includes('/designs') ? "active" : ""}`} onClick={() => navigate('/designs/portfolio')}>GRAPHIC DESIGN</li>
+                    <li className={`${location === '/teaching' ? "active" : ""}`} onClick={() => navigate('/teaching')}>TEACHING</li>
                     <li className={`${location === '/curriculum-vitae' ? "active" : ""}`} onClick={() => navigate('/curriculum-vitae')}>CV</li>
                     <li className={`${location === '/contact' ? "active" : ""}`} onClick={() => navigate('/contact')}>CONTACT</li>
                 </ul>
